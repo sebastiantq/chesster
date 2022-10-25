cecd90f3f214a282cfb8d481900db37b3fd37836
sebastiantq-protonmail <sebastiantq@protonmail.com>
2022-10-25 11:23:36 -0500

    Fixed shmerges

--- src/components/Informacion/Historia/Modal_Images/ModalButton.jsx
+++ src/components/Informacion/Historia/Modal_Images/ModalButton.jsx
@@ -36,11 +36,7 @@ const ModalButton = (props) => {
                 <img src={props.src_img} alt={props.alt_img} className="Img-Modal"></img>
             </Link>
             <p className="Description">{props.img_description}</p>
-<<<<<<< HEAD:src/components/Informacion/Historia/Modal_Images/Modal_Button.jsx
-            { popup === "modal" && <Modal_Image src_img={props.src_img} alt="Img" parrafo={props.paragraph} title={props.title} setPopup={ setPopup }/> }
-=======
             { popup === "modal" && <ModalImage src_img={props.src_img} alt="Img" parrafo={props.img_description} setPopup={ setPopup }/> }
->>>>>>> 5e8184dd84c480f9374e2017fbb1348ca634e316:src/components/Informacion/Historia/Modal_Images/ModalButton.jsx
         </div>
     );
 }
